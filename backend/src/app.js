732886d773ab563221b35a75ccb604bf2bb27443
SumitVishnu <sumitvishnu10@gmail.com>
2026-06-11 00:03:57 +0530

    Setup Express backend with nodemon

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    project: "CHITTI 4.0",
+    status: "Backend Running"
+  });
+});
+
+module.exports = app;
